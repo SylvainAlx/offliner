96c7be506e334e05a816b28830c89e0b948f3884
SylvainAlx <sylvainalx.dev@gmail.com>
2025-09-06 14:55:13 +0200

    Refactor download URL handling and update footer layout

--- shared/config.ts
+++ shared/config.ts
@@ -4,8 +4,6 @@ export const PROJECT = {
   DESCRIPTION:
     "L'application mobile qui compte et encourage le temps passé hors ligne",
   SLOGAN: "Déconnectez pour mieux vous reconnecter",
-  DOWNLOAD_URL:
-    "https://github.com/SylvainAlx/offliner/releases/download/v0.0.1/Offliner_0.0.1.apk",
 };
 
 export const OWNER = {
